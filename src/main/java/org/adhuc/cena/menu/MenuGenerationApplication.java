5de9765cd0c470ae9c3b0d7e9f8e9cafb4cb186c
Alexandre Carbenay <acarbenay@adhuc.fr>
2018-12-27 13:56:37 +0100

    Initialize project from spring initializer

---
+++ src/main/java/org/adhuc/cena/menu/MenuGenerationApplication.java
@@ -0,0 +1,14 @@
+package org.adhuc.cena.menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuGenerationApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/adhuc/cena/menu/MenuGenerationApplicationTests.java
@@ -0,0 +1,17 @@
+package org.adhuc.cena.menu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MenuGenerationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

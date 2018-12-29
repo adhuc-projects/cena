0daa973de50e1a12542897f131c733ae5ce2c470
Alexandre Carbenay <acarbenay@adhuc.fr>
2018-12-29 22:33:04 +0100

    Use JUnit5 to run unit and acceptance tests
    Close #1

--- src/acceptance/java/org/adhuc/cena/menu/HealthAcceptanceTest.java
+++ src/acceptance/java/org/adhuc/cena/menu/HealthAcceptanceTest.java
@@ -1,6 +1,6 @@
 package org.adhuc.cena.menu;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;

e2ba83126b5748d4ef2f541a080c2ff6ea658b7b
Alexandre Carbenay <acarbenay@adhuc.fr>
2019-01-09 22:39:37 +0100

    Modularize project
    Close #10

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+open module org.adhuc.cena.menu.generation {
+    requires spring.beans;
+    requires spring.context;
+    requires spring.core;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+}

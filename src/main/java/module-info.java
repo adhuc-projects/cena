b417c79716b7eb2b6f0b8d08a0f68503817568aa
Alexandre Carbenay <acarbenay@adhuc.fr>
2019-02-11 14:53:59 +0100

    Deactivate java modules to enable lombok usage
    Close #15

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-open module org.adhuc.cena.menu.generation {
-    requires spring.beans;
-    requires spring.context;
-    requires spring.core;
-    requires spring.boot;
-    requires spring.boot.autoconfigure;
-}

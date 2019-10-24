7d644ff33af1b9a9ccd3b68aca88d5e4d2930494
Alexandre Carbenay <acarbenay@adhuc.fr>
2019-10-24 22:21:48 +0200

    chore: Configure java modules for main and test sources
    Close #16

---
+++ src/main/java/module-info.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2019 Alexandre Carbenay
+ *
+ * This file is part of Cena Project.
+ *
+ * Cena Project is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Cena Project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Cena Project. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+open module org.adhuc.cena.menu {
+    requires static lombok;
+    requires com.fasterxml.jackson.annotation;
+    requires java.validation;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.core;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.hateoas;
+    requires spring.security.config;
+    requires spring.security.core;
+    requires spring.web;
+
+    exports org.adhuc.cena.menu;
+}
